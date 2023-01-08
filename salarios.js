e7b2bdbdb28b06bdbf07bb16de34a57e65ed30c1
Ernestoc14 <descreed34@hotmail.com>
2023-01-07 23:50:18 -0500

    Add Files Salarios

---
+++ salarios.js
@@ -0,0 +1,14 @@
+const panama = [];
+
+panama.push({
+    name: "Carlos",
+    salary: 1000,
+});
+panama.push({
+    name: "Nicole",
+    salary: 2500,
+});
+panama.push({
+    name: "Juan",
+    salary: 1800,
+});
