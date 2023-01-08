bcd8bf85dfff2f0f0a03dd6ecc5d188b25a8bbb7
Ernestoc14 <descreed34@hotmail.com>
2023-01-08 01:23:56 -0500

    Add Functions

--- salarios.js
+++ salarios.js
@@ -12,3 +12,31 @@ panama.push({
     name: "Juan",
     salary: 1800,
 });
+panama.push({
+    name: "Ramon",
+    salary: 2000,
+});
+panama.push({
+    name: "Luisa",
+    salary: 900,
+});
+panama.push({
+    name: "Dario",
+    salary: 2400,
+});
+panama.push({
+    name: "Maria",
+    salary: 750,
+});
+panama.push({
+    name: "Lazlo",
+    salary: 350,
+});
+panama.push({
+    name: "Arturo",
+    salary: 3000,
+});
+panama.push({
+    name: "Tito",
+    salary: 2700,
+});
